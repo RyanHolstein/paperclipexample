6e8e97003842396c1a5c7a8b8e3ab6f1d1a7aaa0
Ryan Holstein <ryan.holstein@gmail.com>
2014-03-09 18:10:01 -0700

    began paper clip

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clip::Application.config.secret_key_base = '7b992b519f8259c65d0c482ae447f0e8b77b7e5612534e9465449e7d576655c1308b954535f1754e8bc1117a94bbe7a333526cff9816ae27a7d2fb9ddb525ca4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Clip::Application.config.session_store :cookie_store, key: '_Clip_session'
